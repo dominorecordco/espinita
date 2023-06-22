6913c1aa171ea5ed2c09745d418144955ed0a32c
Richard Worrall <richard@soupish.net>
2023-06-22 13:13:19 +0100

    Resolve Rails 6 autoloading deprecation

--- lib/espinita/engine.rb
+++ lib/espinita/engine.rb
@@ -10,8 +10,12 @@ module Espinita
     end
 
     initializer "include Auditor request into action controller" do |app|
-      ActionController::Base.send(:include, Espinita::AuditorRequest)
-      ActiveRecord::Base.send(:include, Espinita::Auditor)
+      ActiveSupport.on_load :action_controller do
+        include Espinita::AuditorRequest
+      end
+      ActiveSupport.on_load :active_record do
+        include Espinita::Auditor
+      end
     end
 
 
